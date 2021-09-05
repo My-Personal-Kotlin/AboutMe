713781750eb9eff521c804f2e85c30510f2af8fe
Awais Mansha <awaismansha1998@gmail.com>
2021-09-05 06:59:32 +0500

    working on Lesson 2 in Kotlin

--- app/src/main/java/com/aboutme/MainActivity.kt
+++ app/src/main/java/com/aboutme/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicNameTextView:TextView = findViewById(R.id.nickname_text)
+
+        view.visibility = View.GONE
+        editText.visibility = View.GONE
+        nicNameTextView.visibility = View.VISIBLE
+        nicNameTextView.text = editText.text
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
     }
 }
